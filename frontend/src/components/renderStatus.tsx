7096302983064b0c793ade5da5bd5327a2bbbb0d
Vegardhgr <vegard.groder@gmail.com>
2024-02-24 12:56:34 +0100

    Minor change in status rendering

--- frontend/src/components/renderStatus.tsx
+++ frontend/src/components/renderStatus.tsx
@@ -1,10 +1,11 @@
 import CalcWPM from "../utils/calcWPM"
 
 function renderStatus(nrOfCorrectChars:number, nrOfWrongChars:number, initialTimeInSec:number) {
+    const accuracy = (nrOfCorrectChars/(nrOfCorrectChars+nrOfWrongChars)*100).toFixed(1)
     return <div>Correct: <span style={{color:"green"}}>{nrOfCorrectChars}</span>
         Wrong: <span style={{color:"red"}}>{nrOfWrongChars}</span>
         WPM: {CalcWPM(nrOfCorrectChars, initialTimeInSec)}
-        Accuracy: {(nrOfCorrectChars/(nrOfCorrectChars+nrOfWrongChars)).toFixed(2)}</div>
+        Accuracy: {accuracy == "NaN" ? 0 : accuracy}%</div>
 }
 
 export default renderStatus;
