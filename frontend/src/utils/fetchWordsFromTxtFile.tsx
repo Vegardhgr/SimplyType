e1c7a4f45abde9a6c6ccd7ef6e4d89c61466023d
Vegardhgr <vegard.groder@gmail.com>
2024-03-09 22:09:58 +0100

    Cleaned up fetchWordsFromTxtFile.tsx

--- frontend/src/utils/fetchWordsFromTxtFile.tsx
+++ frontend/src/utils/fetchWordsFromTxtFile.tsx
@@ -1,12 +1,8 @@
 import { Dispatch, SetStateAction } from 'react'
-import RandSortArr from './randSortArr';
-// import wordlist from '../wordlists/wordlist_eng.txt'
-
 
 type Dispatcher<S> = Dispatch<SetStateAction<S>>;
-type wordTupleType = [string, boolean | undefined, boolean]
 
-async function FetchWordsFromTxtFile(setUniqueWords:Dispatcher<String[]>, setWordlist:Dispatcher<wordTupleType[]>, language:string) {
+async function FetchWordsFromTxtFile(setUniqueWords:Dispatcher<String[]>, language:string) {
     const wordlist = "/wordlists/wordlist_"+language+".txt"
     try {
         const response = await fetch(wordlist)
