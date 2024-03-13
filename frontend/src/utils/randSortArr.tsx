2b8fb019a26dbd1d487373e2dc53368a0863bbcc
Vegardhgr <vegard.groder@gmail.com>
2024-03-13 15:45:49 +0100

    Only fetching 20 random words at a time

--- frontend/src/utils/randSortArr.tsx
+++ frontend/src/utils/randSortArr.tsx
@@ -1,10 +1,17 @@
 
 function RandSortArr(arr: String[]) {
-    for (let i: number = arr.length-1; i > 0; i--) {
+    /*for (let i: number = arr.length-1; i > 0; i--) {
         const j: number = Math.floor(Math.random()*(i+1));
         [arr[i], arr[j]] = [arr[j], arr[i]]
+    }*/
+
+    const randomWordsFromArr: String[] = []
+    const numberOfWords = 20
+
+    for (let i = 0; i<numberOfWords; i++) {
+        randomWordsFromArr.push(arr[Math.floor(Math.random() * arr.length)])
     }
-    return arr
+    return randomWordsFromArr
 }
 
 
